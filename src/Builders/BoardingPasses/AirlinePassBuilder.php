dbfce0bf9183b8e5952552f843bceef1c4d10e9a
Dan Johnson <dan@try.be>
2025-03-12 01:34:22 +0530

    feat: create boarding pass builders

---
+++ src/Builders/BoardingPasses/AirlinePassBuilder.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Spatie\LaravelMobilePass\Builders\BoardingPasses;
+
+use Spatie\LaravelMobilePass\Builders\BoardingPasses\BoardingPassBuilder;
+use Spatie\LaravelMobilePass\Enums\TransitType;
+
+class AirlinePassBuilder extends BoardingPassBuilder
+{
+    protected ?TransitType $transitType = TransitType::Air;
+
+    protected ?string $airlineCode = null;
+    protected ?string $flightCode = null;
+    protected ?string $flightNumber = null;
+
+    protected ?string $departureGate = null;
+    protected ?string $departureTerminal = null;
+    protected ?string $departureAirportCode = null;
+    protected ?string $departureAirportName = null;
+
+    protected ?string $destinationAirportName = null;
+    protected ?string $destinationAirportCode = null;
+    protected ?string $destinationGate = null;
+    protected ?string $destinationTerminal = null;
+
+
+    /**
+     * The IATA airline code, such as EX for flightCode EX123.
+     */
+    public function setAirlineCode(string $airlineCode): self
+    {
+        $this->airlineCode = $airlineCode;
+
+        return $this;
+    }
+
+    /**
+     * The IATA airport code for the departure airport, such as MPM or LHR.
+     */
+    public function setDepartureAirportCode(string $departureAirportCode): self
+    {
+        $this->departureAirportCode = $departureAirportCode;
+
+        return $this;
+    }
+
+    /**
+     * The full name of the departure airport, such as Maputo International Airport.
+     */
+    public function setDepartureAirportName(string $departureAirportName): self
+    {
+        $this->departureAirportName = $departureAirportName;
+
+        return $this;
+    }
+
+    /**
+     * The gate number or letters of the departure gate, such as 1A. Don’t include the word gate.
+     */
+    public function setDepartureGate(string $departureGate): self
+    {
+        $this->departureGate = $departureGate;
+
+        return $this;
+    }
+
+    /**
+     * The name or letter of the departure terminal, such as A. Don’t include the word terminal.
+     */
+    public function setDepartureTerminal(string $departureTerminal): self
+    {
+        $this->departureTerminal = $departureTerminal;
+
+        return $this;
+    }
+
+    /**
+     * The full name of the destination airport, such as London Heathrow.
+     */
+    public function setDestinationAirportName(string $destinationAirportName): self
+    {
+        $this->destinationAirportName = $destinationAirportName;
+
+        return $this;
+    }
+
+    /**
+     * The IATA airport code for the destination airport, such as MPM or LHR.
+     */
+    public function setDestinationAirportCode(string $destinationAirportCode): self
+    {
+        $this->destinationAirportCode = $destinationAirportCode;
+
+        return $this;
+    }
+
+    /**
+     * The gate number or letter of the destination gate, such as 1A. Don’t include the word gate.
+     */
+    public function setDestinationGate(string $destinationGate): self
+    {
+        $this->destinationGate = $destinationGate;
+
+        return $this;
+    }
+
+    /**
+     * The terminal name or letter of the destination terminal, such as A. Don’t include the word terminal.
+     */
+    public function setDestinationTerminal(string $destinationTerminal): self
+    {
+        $this->destinationTerminal = $destinationTerminal;
+
+        return $this;
+    }
+
+    /**
+     * The IATA flight code, such as EX123.
+     */
+    public function setFlightCode(string $flightCode): self
+    {
+        $this->flightCode = $flightCode;
+
+        return $this;
+    }
+
+    /**
+     * The numeric portion of the IATA flight code, such as 123 for flightCode EX123.
+     */
+    public function setFlightNumber(string $flightNumber): self
+    {
+        $this->flightNumber = $flightNumber;
+
+        return $this;
+    }
+
+}
---
+++ src/Builders/BoardingPasses/BoardingPassBuilder.php
@@ -0,0 +1,390 @@
+<?php
+
+namespace Spatie\LaravelMobilePass\Builders\BoardingPasses;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+use Spatie\LaravelMobilePass\Builders\PassBuilder;
+use Spatie\LaravelMobilePass\Entities\Image;
+use Spatie\LaravelMobilePass\Entities\Location;
+use Spatie\LaravelMobilePass\Entities\PersonName;
+use Spatie\LaravelMobilePass\Entities\Seat;
+use Spatie\LaravelMobilePass\Enums\PassType;
+use Spatie\LaravelMobilePass\Enums\TransitType;
+use Spatie\LaravelMobilePass\Validators\BoardingPassValidator;
+use Spatie\LaravelMobilePass\Validators\PassValidator;
+
+abstract class BoardingPassBuilder extends PassBuilder
+{
+    protected PassType $type = PassType::BoardingPass;
+    protected ?TransitType $transitType = null;
+
+    protected ?Carbon $currentArrivalDate = null;
+    protected ?Carbon $currentBoardingDate = null;
+    protected ?Carbon $currentDepartureDate = null;
+
+    protected ?Carbon $originalArrivalDate = null;
+    protected ?Carbon $originalBoardingDate = null;
+    protected ?Carbon $originalDepartureDate = null;
+
+    protected ?string $confirmationNumber = null;
+    protected ?string $boardingGroup = null;
+    protected ?string $boardingSequenceNumber = null;
+
+    protected ?Location $departureLocation = null;
+    protected ?string $departureLocationDescription = null;
+
+    protected ?Location $destinationLocation = null;
+    protected ?string $destinationLocationDescription = null;
+
+    protected ?int $durationInSeconds = null;
+
+    protected ?string $membershipProgramName = null;
+    protected ?string $membershipProgramNumber = null;
+
+    protected ?PersonName $passengerName = null;
+    protected ?string $priorityStatus = null;
+    protected ?Collection $seats = null;
+    protected ?string $securityScreening = null;
+    protected ?bool $silenceRequested = null;
+    protected ?string $transitProvider = null;
+    protected ?string $transitStatus = null;
+    protected ?string $transitStatusReason = null;
+    protected ?string $vehicleName = null;
+    protected ?string $vehicleNumber = null;
+    protected ?string $vehicleType = null;
+
+    protected static function validator(): PassValidator
+    {
+        return new BoardingPassValidator;
+    }
+
+    /**
+     * A group number for boarding.
+     */
+    public function setBoardingGroup(string $boardingGroup): self
+    {
+        $this->boardingGroup = $boardingGroup;
+
+        return $this;
+    }
+
+    /**
+     * A sequence number for boarding.
+     */
+    public function setBoardingSequenceNumber(string $boardingSequenceNumber): self
+    {
+        $this->boardingSequenceNumber = $boardingSequenceNumber;
+
+        return $this;
+    }
+
+    /**
+     * A booking or reservation confirmation number.
+     */
+    public function setConfirmationNumber(string $confirmationNumber)
+    {
+        $this->confirmationNumber = $confirmationNumber;
+
+        return $this;
+    }
+
+    /**
+     * The updated date and time of arrival, if different from the originally scheduled date and time.
+     */
+    public function setCurrentArrivalDate(Carbon $currentArrivalDate): self
+    {
+        $this->currentArrivalDate = $currentArrivalDate;
+
+        return $this;
+    }
+
+    /**
+     * The updated date and time of boarding, if different from the originally scheduled date and time.
+     */
+    public function setCurrentBoardingDate(Carbon $currentBoardingDate): self
+    {
+        $this->currentBoardingDate = $currentBoardingDate;
+
+        return $this;
+    }
+
+    /**
+     * The updated departure date and time, if different from the originally scheduled date and time.
+     */
+    public function currentDepartureDate(Carbon $currentDepartureDate): self
+    {
+        $this->currentDepartureDate = $currentDepartureDate;
+
+        return $this;
+    }
+
+    /**
+     * An object that represents the geographic coordinates of the transit departure location, suitable for display on a map. If possible, use precise locations, which are more useful to travelers; for example, the specific location of an airport gate.
+     */
+    public function setDepartureLocation(Location $departureLocation): self
+    {
+        $this->departureLocation = $departureLocation;
+
+        return $this;
+    }
+
+    /**
+     * A brief description of the departure location. For example, for a flight departing from an airport that has a code of LHR, an appropriate description might be London, Heathrow. 
+     */
+    public function setDepartureLocationDescription(string $departureLocationDescription): self
+    {
+        $this->departureLocationDescription = $departureLocationDescription;
+
+        return $this;
+    }
+
+    /**
+     * An object that represents the geographic coordinates of the transit departure location, suitable for display on a map.
+     */
+    public function setDestinationLocation(Location $destinationLocation): self
+    {
+        $this->destinationLocation = $destinationLocation;
+
+        return $this;
+    }
+
+    /**
+     * A brief description of the destination location. For example, for a flight arriving at an airport that has a code of MPM, Maputo might be an appropriate description.
+     */
+    public function setDestinationLocationDescription(string $destinationLocationDescription): self
+    {
+        $this->destinationLocationDescription = $destinationLocationDescription;
+
+        return $this;
+    }
+
+    /**
+     * The duration of the transit journey, in seconds.
+     */
+    public function setDuration(int $durationInSeconds): self
+    {
+        $this->durationInSeconds = $durationInSeconds;
+
+        return $this;
+    }
+
+    /**
+     * The name of a frequent flyer or loyalty program.
+     */
+    public function setMembershipProgramName(string $membershipProgramName): self
+    {
+        $this->membershipProgramName = $membershipProgramName;
+
+        return $this;
+    }
+
+    /**
+     * The ticketed passenger’s frequent flyer or loyalty number. 
+     */
+    public function setMembershipProgramNumber(string $membershipProgramNumber): self
+    {
+        $this->membershipProgramNumber = $membershipProgramNumber;
+
+        return $this;
+    }
+
+    /**
+     * The originally scheduled date and time of arrival.
+     */
+    public function setOriginalArrivalDate(Carbon $originalArrivalDate): self
+    {
+        $this->originalArrivalDate = $originalArrivalDate;
+
+        return $this;
+    }
+
+    /**
+     * The originally scheduled date and time of boarding. 
+     */
+    public function setOriginalBoardingDate(Carbon $originalBoardingDate): self
+    {
+        $this->originalBoardingDate = $originalBoardingDate;
+
+        return $this;
+    }
+
+    /**
+     * The originally scheduled date and time of departure.
+     */
+    public function setOriginalDepartureDate(Carbon $originalDepartureDate): self
+    {
+        $this->originalDepartureDate = $originalDepartureDate;
+
+        return $this;
+    }
+
+    /**
+     * An object that represents the name of the passenger.
+     */
+    public function setPassengerName(PersonName $passengerName): self
+    {
+        $this->passengerName = $passengerName;
+
+        return $this;
+    }
+
+    /**
+     * The priority status the ticketed passenger holds, such as “Gold” or “Silver”.
+     */
+    public function setPriorityStatus(string $priorityStatus): self
+    {
+        $this->priorityStatus = $priorityStatus;
+
+        return $this;
+    }
+
+    /**
+     * An object that represents the details for each seat on a transit journey.
+     */
+    public function setSeats(Seat ...$seat): self
+    {
+        $this->seats = collect($seat);
+
+        return $this;
+    }
+
+    /**
+     * The type of security screening for the ticketed passenger, such as “Priority”.
+     */
+    public function setSecurityScreening(string $securityScreening): self
+    {
+        $this->securityScreening = $securityScreening;
+
+        return $this;
+    }
+
+    /**
+     * A Boolean value that determines whether the user’s device remains silent during a transit journey. The system may override the key and determine the length of the period of silence.
+     */
+    public function setSilenceRequested(bool $silenceRequested): self
+    {
+        $this->silenceRequested = $silenceRequested;
+
+        return $this;
+    }
+
+    /**
+     * The name of the transit company.
+     */
+    public function setTransitProvider(string $transitProvider): self
+    {
+        $this->transitProvider = $transitProvider;
+
+        return $this;
+    }
+
+    /**
+     * A brief description of the current boarding status for the vessel, such as “On Time” or “Delayed”. For delayed status, provide currentBoardingDate, currentDepartureDate, and currentArrivalDate where available.
+     */
+    public function setTransitStatus(string $transitStatus): self
+    {
+        $this->transitStatus = $transitStatus;
+
+        return $this;
+    }
+
+    /**
+     * A brief description that explains the reason for the current transitStatus, such as “Thunderstorms”.
+     */
+    public function setTransitStatusReason(string $transitStatusReason): self
+    {
+        $this->transitStatusReason = $transitStatusReason;
+
+        return $this;
+    }
+
+    /**
+     * The name of the vehicle to board, such as the name of a boat.
+     */
+    public function setVehicleName(string $vehicleName): self
+    {
+        $this->vehicleName = $vehicleName;
+
+        return $this;
+    }
+
+    /**
+     * The identifier of the vehicle to board, such as the aircraft registration number or train number. 
+     */
+    public function setVehicleNumber(string $vehicleNumber): self
+    {
+        $this->vehicleNumber = $vehicleNumber;
+
+        return $this;
+    }
+
+    /**
+     * A brief description of the type of vehicle to board, such as the model and manufacturer of a plane or the class of a boat.
+     */
+    public function setVehicleType(string $vehicleType): self
+    {
+        $this->vehicleType = $vehicleType;
+
+        return $this;
+    }
+
+    public function setFooterImage(Image $image): self
+    {
+        $this->images['footer'] = $image;
+
+        return $this;
+    }
+
+    protected function compileSemantics(): array
+    {
+        return array_merge(
+            parent::compileSemantics(),
+            array_filter([
+                'boardingGroup' => $this->boardingGroup,
+                'boardingSequenceNumber' => $this->boardingSequenceNumber,
+                'confirmationNumber' => $this->confirmationNumber,
+                'currentArrivalDate' => $this->currentArrivalDate?->toIso8601String(),
+                'currentBoardingDate' => $this->currentBoardingDate?->toIso8601String(),
+                'currentDepartureDate' => $this->currentDepartureDate?->toIso8601String(),
+                'departureLocation' => $this->departureLocation?->toArray(),
+                'departureLocationDescription' => $this->departureLocationDescription,
+                'destinationLocation' => $this->destinationLocation?->toArray(),
+                'destinationLocationDescription' => $this->destinationLocationDescription,
+                'duration' => $this->durationInSeconds,
+                'membershipProgramName' => $this->membershipProgramName,
+                'membershipProgramNumber' => $this->membershipProgramNumber,
+                'originalArrivalDate' => $this->originalArrivalDate?->toIso8601String(),
+                'originalBoardingDate' => $this->originalBoardingDate?->toIso8601String(),
+                'originalDepartureDate' => $this->originalDepartureDate?->toIso8601String(),
+                'passengerName' => $this->passengerName?->toArray(),
+                'priorityStatus' => $this->priorityStatus,
+                'seats' => $this->seats?->toArray(),
+                'securityScreening' => $this->securityScreening,
+                'silenceRequsted' => $this->silenceRequested,
+                'transitProvider' => $this->transitProvider,
+                'transitStatus' => $this->transitStatus,
+                'transitStatusReason' => $this->transitStatusReason,
+                'vehicleName' => $this->vehicleName,
+                'vehicleNumber' => $this->vehicleNumber,
+                'vehicleType' => $this->vehicleType,
+            ]),
+        );
+    }
+
+    protected function compileData(): array
+    {
+        return array_merge(
+            parent::compileData(),
+            [
+                'boardingPass' => array_filter([
+                    'transitType' => $this->transitType?->value,
+                    'primaryFields' => $this->primaryFields?->toArray(),
+                    'secondaryFields' => $this->secondaryFields?->toArray(),
+                    'headerFields' => $this->headerFields?->toArray(),
+                    'auxiliaryFields' => $this->auxiliaryFields?->toArray(),
+                ]),
+            ],
+        );
+    }
+}
