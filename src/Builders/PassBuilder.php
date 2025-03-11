6a2bf45fcd0ef52a85fffbe7a2f71a22fde00c17
Dan Johnson <dan@try.be>
2025-03-12 01:33:56 +0530

    feat: build first draft of PassBuilder

---
+++ src/Builders/PassBuilder.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace Spatie\LaravelMobilePass\Builders;
+
+use Illuminate\Support\Collection;
+use PKPass\PKPass;
+use Spatie\LaravelMobilePass\Entities\FieldContent;
+use Spatie\LaravelMobilePass\Entities\Image;
+use Spatie\LaravelMobilePass\Entities\Price;
+use Spatie\LaravelMobilePass\Entities\WifiNetwork;
+use Spatie\LaravelMobilePass\Enums\PassType;
+use Spatie\LaravelMobilePass\Validators\PassValidator;
+
+abstract class PassBuilder
+{
+    protected PassType $type;
+
+    protected ?string $serialNumber = null;
+    protected ?string $organisationName = null;
+    protected ?string $description = null;
+    protected ?Price $totalPrice = null;
+    protected ?Collection $wifiDetails = null;
+
+    protected ?Collection $primaryFields = null;
+    protected ?Collection $secondaryFields = null;
+    protected ?Collection $auxiliaryFields = null;
+    protected ?Collection $headerFields = null;
+    protected ?Collection $backFields = null;
+
+    protected array $images = [];
+
+    protected static function validator()
+    {
+        return new PassValidator();
+    }
+
+    public static function make(): static
+    {
+        return new static();
+    }
+
+    public function setLogoImage(Image $image): self
+    {
+        $this->images['logo'] = $image;
+
+        return $this;
+    }
+
+    public function setIconImage(Image $image): self
+    {
+        $this->images['icon'] = $image;
+
+        return $this;
+    }
+
+    public function setPrimaryFields(FieldContent ...$primaryField): self
+    {
+        $this->primaryFields = collect($primaryField);
+
+        return $this;
+    }
+
+    public function setSecondaryFields(FieldContent ...$secondaryField): self
+    {
+        $this->secondaryFields = collect($secondaryField);
+
+        return $this;
+    }
+
+    public function setAuxiliaryFields(FieldContent ...$auxiliaryField): self
+    {
+        $this->auxiliaryFields = collect($auxiliaryField);
+
+        return $this;
+    }
+
+    public function setHeaderFields(FieldContent ...$headerField): self
+    {
+        $this->headerFields = collect($headerField);
+
+        return $this;
+    }
+
+    public function setSerialNumber(string $serialNumber): self
+    {
+        $this->serialNumber = $serialNumber;
+
+        return $this;
+    }
+
+    public function setOrganisationName(string $organisationName): self
+    {
+        $this->organisationName = $organisationName;
+
+        return $this;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * The total price for the pass.
+     */
+    public function setTotalPrice(Price $totalPrice): self
+    {
+        $this->totalPrice = $totalPrice;
+
+        return $this;
+    }
+
+    public function setWifiDetails(WifiNetwork ...$wifiNetwork): self
+    {
+        $this->wifiDetails = collect($wifiNetwork);
+
+        return $this;
+    }
+
+    protected function compileSemantics(): ?array
+    {
+        return array_filter([
+            'totalPrice' => $this->totalPrice?->toArray(),
+            'wifiAccess' => $this->wifiDetails?->toArray(),
+        ]);
+    }
+
+    protected function compileData(): array
+    {
+        return [
+            'formatVersion' => 1,
+            'organizationName' => config('mobile-pass.organisation_name'),
+            'passTypeIdentifier' => config('mobile-pass.type_identifier'),
+            'serialNumber' => $this->serialNumber,
+            'authenticationToken' => config('mobile-pass.webservice_secret'),
+            'teamIdentifier' => config('mobile-pass.team_identifier'),
+            'description' => $this->description,
+            'semantics' => $this->compileSemantics(),
+        ];
+    }
+
+    protected function addImagesToFile(PKPass $pkPass): PKPass
+    {
+        foreach ($this->images as $filename => $image)
+        {
+            // The $image Image entity could contain up to three
+            // images in different resolutions.
+
+            if ($image->x1Path) {
+                $pkPass->addFile($image->x1Path, "$filename.png");
+            }
+
+            if ($image->x2Path) {
+                $pkPass->addFile($image->x2Path, "$filename@2x.png");
+            }
+
+            if ($image->x3Path) {
+                $pkPass->addFile($image->x3Path, "$filename@3x.png");
+            }
+        }
+
+        return $pkPass;
+    }
+
+    public function generate()
+    {
+        // Remove any null keys or keys where the value is an empty array.
+        // TODO: do this recursively.
+        $compiledData = array_filter(
+            $this->compileData(),
+            fn ($value) => !empty($value)
+        );
+
+        $data = $this->validator()->validate(
+            $compiledData
+        );
+
+        // The icon image is always required.
+        // TODO: validate this.
+
+        $pkPass = new PKPass(
+            config('mobile-pass.apple.certificate_path'),
+            config('mobile-pass.apple.certificate_password'),
+        );
+
+        $pkPass->setData($data);
+
+        $this->addImagesToFile($pkPass);
+
+        return $pkPass->create(output: false);        
+    }
+}
