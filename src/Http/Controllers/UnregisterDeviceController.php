5ad9cf3dea980582b1d377a9a4f2df8e35182209
Dan Johnson <dan@try.be>
2025-03-12 04:33:00 +0530

    feat: add stubs for controllers

---
+++ src/Http/Controllers/UnregisterDeviceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Spatie\LaravelMobilePass\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class UnregisterDeviceController extends Controller
+{
+    public function __invoke()
+    {
+        
+    }
+}
