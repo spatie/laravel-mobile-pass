98db2c832974ca91e5b93dbc7d0d8aa023a779a9
Dan Johnson <dan@try.be>
2025-03-17 16:36:53 +0000

    fix: set environment vars correctly

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace Spatie\LaravelMobilePass\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Spatie\LaravelMobilePass\LaravelMobilePassServiceProvider;
 
@@ -12,8 +11,6 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        $this->setDefaultValuesInConfig();
 
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'Spatie\\LaravelMobilePass\\Database\\Factories\\'.class_basename($modelName).'Factory'
@@ -25,35 +22,5 @@ class TestCase extends Orchestra
         return [
             LaravelMobilePassServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        $app->useEnvironmentPath(__DIR__.'/..');
-        $app->bootstrapWith([LoadEnvironmentVariables::class]);
-
-        /*
-         foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/database/migrations') as $migration) {
-            (include $migration->getRealPath())->up();
-         }
-         */
-    }
-
-    protected function setDefaultValuesInConfig(): self
-    {
-        config()->set('mobile-pass', [
-            'organisation_name' => 'Organisation name',
-            'type_identifier' => 'Type identifier',
-            'team_identifier' => 'Team identifier',
-            'apple' => [
-                'certificate_path' => 'dummy path',
-                'certificate_contents' => 'dummy contents',
-                'certificate_password' => 'dummy password',
-            ],
-        ]);
-
-        return $this;
     }
 }
