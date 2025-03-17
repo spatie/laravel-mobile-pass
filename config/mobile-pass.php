443f3d710b2cf126119f02d8d3b3d06f20f61514
Dan Johnson <dan@try.be>
2025-03-17 22:30:32 +0000

    feat: add config var for apple_push_base_url

--- config/mobile-pass.php
+++ config/mobile-pass.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    'organisation_name' => env('MOBILE_PASS_ORGANISATION_NAME'),
+    'organisation_name' => env('MOBILE_PASS_ORGANISATION_NAME', 'Spatie'),
     'type_identifier' => env('MOBILE_PASS_TYPE_IDENTIFIER'),
     'team_identifier' => env('MOBILE_PASS_TEAM_IDENTIFIER'),
     'apple' => [
+        'apple_push_base_url' => 'https://api.push.apple.com/3/device',
         'certificate_path' => env('MOBILE_PASS_APPLE_CERTIFICATE_PATH'),
         'certificate_contents' => env('MOBILE_PASS_APPLE_CERTIFICATE_CONTENTS'),
         'certificate_password' => env('MOBILE_PASS_APPLE_CERTIFICATE_PASSWORD'),
