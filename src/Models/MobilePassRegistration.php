428c1995b2002e96c71ded54a6c79607c8184657
Dan Johnson <dan@try.be>
2025-03-19 18:51:47 +0000

    feat: soft-deletable registrations

--- src/Models/MobilePassRegistration.php
+++ src/Models/MobilePassRegistration.php
@@ -2,19 +2,23 @@
 
 namespace Spatie\LaravelMobilePass\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\LaravelMobilePass\Support\Config;
 
 class MobilePassRegistration extends Model
 {
+    use HasUuids, SoftDeletes;
+
     public $guarded = [];
 
     public function pass(): BelongsTo
     {
         $modelClass = Config::mobilePassModel();
 
-        return $this->belongsTo($modelClass, 'id', 'pass_serial');
+        return $this->belongsTo($modelClass, 'pass_serial');
     }
 
     public function appleUpdateUrl(): string
