0d6cd27d05ee1dcdebd05a3bf9bd74caf0f38f92
Dan Johnson <dan@try.be>
2025-03-19 19:06:41 +0000

    feat: support different pass types

--- src/Models/MobilePass.php
+++ src/Models/MobilePass.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use PKPass\PKPass;
 use Spatie\LaravelMobilePass\Actions\NotifyAppleOfPassUpdateAction;
+use Spatie\LaravelMobilePass\Entities\Colour;
 use Spatie\LaravelMobilePass\Entities\FieldContent;
 use Spatie\LaravelMobilePass\Entities\Image;
+use Spatie\LaravelMobilePass\Enums\PassType;
 use Spatie\LaravelMobilePass\Enums\TransitType;
 use Spatie\LaravelMobilePass\Support\Config;
 
@@ -34,7 +36,20 @@ class MobilePass extends Model
 
     public array $auxiliaryFields = [];
 
-    public array $images = [];
+    public ?Colour $backgroundColour = null;
+
+    public ?Colour $labelColour = null;
+
+    public array $passImages = [];
+
+    public PassType $passType = PassType::Generic;
+
+    public function setType(PassType $passType): self
+    {
+        $this->passType = $passType;
+
+        return $this;
+    }
 
     public static function boot()
     {
@@ -72,25 +87,30 @@ class MobilePass extends Model
 
     protected static function uncompileContent(MobilePass $model)
     {
-        $passType = 'boardingPass'; // TODO: make this dynamic
+        $passType = PassType::tryFrom($model->content['userInfo']['passType'] ?? PassType::Generic);
 
         $model->organisationName = $model->content['organisationName'] ?? null;
         $model->passTypeIdentifier = $model->content['passTypeIdentifier'] ?? null;
         $model->authenticationToken = $model->content['authenticationToken'] ?? null;
         $model->teamIdentifier = $model->content['teamIdentifier'] ?? null;
         $model->description = $model->content['description'] ?? null;
+        $model->backgroundColour = Colour::makeFromRgbString($model->content['backgroundColor'] ?? null);
+        $model->labelColour = Colour::makeFromRgbString($model->content['labelColor'] ?? null);
+        $model->passType = $passType;
 
-        $model->headerFields = array_map(fn ($field) => FieldContent::fromArray($field), $model->content[$passType]['headerFields'] ?? []);
-        $model->primaryFields = array_map(fn ($field) => FieldContent::fromArray($field), $model->content[$passType]['primaryFields'] ?? []);
-        $model->secondaryFields = array_map(fn ($field) => FieldContent::fromArray($field), $model->content[$passType]['secondaryFields'] ?? []);
-        $model->auxiliaryFields = array_map(fn ($field) => FieldContent::fromArray($field), $model->content[$passType]['auxiliaryFields'] ?? []);
+        $model->passImages = array_map(fn ($image) => Image::fromArray($image), $model->images);
+
+        $model->headerFields = array_map(fn ($field) => FieldContent::fromArray($field), $model->content[$passType->value]['headerFields'] ?? []);
+        $model->primaryFields = array_map(fn ($field) => FieldContent::fromArray($field), $model->content[$passType->value]['primaryFields'] ?? []);
+        $model->secondaryFields = array_map(fn ($field) => FieldContent::fromArray($field), $model->content[$passType->value]['secondaryFields'] ?? []);
+        $model->auxiliaryFields = array_map(fn ($field) => FieldContent::fromArray($field), $model->content[$passType->value]['auxiliaryFields'] ?? []);
     }
 
     protected static function compileContent(MobilePass $model)
     {
-        $passType = 'boardingPass'; // TODO: make this dynamic
+        $model->images = $model->passImages;
 
-        $model->content = [
+        $model->content = array_filter([
             'formatVersion' => 1,
             'organizationName' => $model->organisationName ?? config('mobile-pass.organisation_name'),
             'passTypeIdentifier' => $model->passTypeIdentifier ?? config('mobile-pass.type_identifier'),
@@ -99,8 +119,14 @@ class MobilePass extends Model
             'teamIdentifier' => $model->teamIdentifier ?? config('mobile-pass.team_identifier'),
             'description' => $model->description,
             'serialNumber' => $model->getKey(),
-            $passType => self::compileFields($model),
-        ];
+            'backgroundColor' => (string) $model->backgroundColour,
+            'labelColor' => (string) $model->labelColour,
+            'userInfo' => [
+                'passType' => $model->passType->value,
+            ],
+
+            $model->passType->value => self::compileFields($model),
+        ]);
     }
 
     protected static function compileFields(MobilePass $model)
@@ -146,14 +172,28 @@ class MobilePass extends Model
 
     public function setLogoImage(Image $image): self
     {
-        $this->images['logo'] = $image;
+        $this->passImages['logo'] = $image;
 
         return $this;
     }
 
     public function setIconImage(Image $image): self
     {
-        $this->images['icon'] = $image;
+        $this->passImages['icon'] = $image;
+
+        return $this;
+    }
+
+    public function setBackgroundColour(Colour $colour): self
+    {
+        $this->backgroundColour = $colour;
+
+        return $this;
+    }
+
+    public function setLabelColour(Colour $colour): self
+    {
+        $this->labelColour = $colour;
 
         return $this;
     }
@@ -179,9 +219,16 @@ class MobilePass extends Model
         return $this;
     }
 
+    public function addAuxiliaryFields(FieldContent ...$fieldContent)
+    {
+        $this->auxiliaryFields = $fieldContent;
+
+        return $this;
+    }
+
     protected function addImagesToFile(PKPass $pkPass): PKPass
     {
-        foreach ($this->images as $filename => $image) {
+        foreach ($this->passImages as $filename => $image) {
             // The $image Image entity could contain up to three
             // images in different resolutions.
 
