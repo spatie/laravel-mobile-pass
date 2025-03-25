fcf305e1ddc915d8869429cb81c9dd9b69497ac7
Dan Johnson <dan@try.be>
2025-03-25 21:47:59 +0000

    feat: create Template model

---
+++ src/Models/Template.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Spatie\LaravelMobilePass\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\LaravelMobilePass\Entities\Barcode;
+use Spatie\LaravelMobilePass\Entities\Colour;
+use Spatie\LaravelMobilePass\Entities\FieldContent;
+use Spatie\LaravelMobilePass\Entities\Image;
+use Spatie\LaravelMobilePass\Enums\PassType;
+use Spatie\LaravelMobilePass\Enums\TransitType;
+use Spatie\LaravelMobilePass\Models\Traits\HasPassData;
+
+class Template extends Model
+{
+    use HasUuids, HasPassData;
+
+    public $table = 'mobile_pass_templates';
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::retrieved(function (self $mobilePass) {
+            self::uncompileContent($mobilePass);
+        });
+
+        // static::updated(function (MobilePass $mobilePass) {
+        //     /** @var class-string<NotifyAppleOfPassUpdateAction> $action */
+        //     // $action = Config::getActionClass('notify_apple_of_pass_update', NotifyAppleOfPassUpdateAction::class);
+
+        //     // app($action)->execute($mobilePass);
+        // });
+
+        static::saving(function (self $mobilePass) {
+            self::compileContent($mobilePass);
+        });
+    }
+
+    public static function compileContent(self $model)
+    {
+        $model->images = $model->passImages;
+
+        $model->content = array_filter([
+            'formatVersion' => 1,
+            'organizationName' => $model->organisationName ?? config('mobile-pass.organisation_name'),
+            'passTypeIdentifier' => $model->passTypeIdentifier ?? config('mobile-pass.type_identifier'),
+            'authenticationToken' => config('mobile-pass.apple.webservice.secret'),
+            'webServiceURL' => config('mobile-pass.apple.webservice.host').'/passkit/',
+            'teamIdentifier' => $model->teamIdentifier ?? config('mobile-pass.team_identifier'),
+            'description' => $model->description,
+            'serialNumber' => $model->getKey(),
+            'backgroundColor' => (string) $model->backgroundColour,
+            'foregroundColor' => (string) $model->foregroundColour,
+            'labelColor' => (string) $model->labelColour,
+            'barcodes' => array_map(fn ($barcode) => $barcode->toArray(), $model->barcodes),
+            'voided' => $model->voided,
+            'userInfo' => [
+                'passType' => $model->passType->value,
+            ],
+
+            $model->passType->value => self::compileFields($model),
+        ]);
+    }
+
+    public function passes()
+    {
+        return $this->hasMany(MobilePass::class);
+    }
+}
