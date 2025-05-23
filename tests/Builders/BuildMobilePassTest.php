3933dc7644fb79f10ce32fb4ca71cdf42bcf01dd
freekmurze <483853+freekmurze@users.noreply.github.com>
2025-05-23 13:02:15 +0000

    Fix styling

--- tests/Builders/BuildMobilePassTest.php
+++ tests/Builders/BuildMobilePassTest.php
@@ -5,7 +5,6 @@ namespace Spatie\LaravelMobilePass\Tests\Feature;
 use Spatie\LaravelMobilePass\Builders\Apple\Entities\FieldContent;
 use Spatie\LaravelMobilePass\Builders\Apple\Entities\Image;
 use Spatie\LaravelMobilePass\Builders\Apple\GenericPassBuilder;
-use Spatie\LaravelMobilePass\Models\MobilePass;
 
 it('can create a mobile pass', function () {
     $pass = GenericPassBuilder::make()
